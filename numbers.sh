9c6025a98b441b2dc7eb9a3b1da9d43ecafe31fc
Louis Condevaux <condevaux@chapman.edu>
2019-11-11 10:38:09 -0800

    numbers finished

---
+++ numbers.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+#numbers.sh
+#Louis Condevaux
+
+echo "Enter the number you want to check"
+read  number
+
+N=1
+while  [ "$N" -le "$number" ] > /dev/null 2>&1
+do 
+	if [ $((N%2)) -eq 0 ]  
+	then
+		echo "$N number is even"
+	else
+		echo "$N number is odd"
+	fi
+N=$[$N+1]
+done
+	
+	 
